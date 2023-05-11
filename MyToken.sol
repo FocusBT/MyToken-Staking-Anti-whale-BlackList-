57353aa7f99df8d023e0c32691fa425a2b1cdb37
waseeABD <76704015+FocusBT@users.noreply.github.com>
2023-05-11 10:04:49 +0500

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -26,17 +26,9 @@ contract MyToken is ERC20, Ownable {
 
     
 
-    function test() public{
-        approve(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 1000);
-    }
-
-    function test2() public{
-        transferFrom(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 1000);
-        // 935   // 765
-        // 1065
         
     }
-    uint public testing = 0;
+    
     uint public Liquidity;
     uint public wheel;
     uint public reflections;
